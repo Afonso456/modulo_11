c6d4434bb672552911ac456cdd4f9305e16925d9
Afonso Costa <ac0009055@gmail.com>
2026-01-23 08:51:48 +0000

    first repo

--- gerir_pessoas/gerir_pessoas/Form1.Designer.cs
+++ gerir_pessoas/gerir_pessoas/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             btn_guardat = new Button();
             btn_lert = new Button();
-            listBox1 = new ListBox();
+            lst_pessoas = new ListBox();
             button1 = new Button();
             button2 = new Button();
             txt_id = new TextBox();
@@ -41,11 +41,14 @@
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
+            btn_converter_TJ = new Button();
+            btn_converter_JT = new Button();
+            btn_maior_18 = new Button();
             SuspendLayout();
             // 
             // btn_guardat
             // 
-            btn_guardat.Location = new Point(28, 267);
+            btn_guardat.Location = new Point(12, 303);
             btn_guardat.Name = "btn_guardat";
             btn_guardat.Size = new Size(95, 20);
             btn_guardat.TabIndex = 0;
@@ -55,63 +58,66 @@
             // 
             // btn_lert
             // 
-            btn_lert.Location = new Point(30, 293);
+            btn_lert.Location = new Point(113, 303);
             btn_lert.Name = "btn_lert";
             btn_lert.Size = new Size(95, 20);
             btn_lert.TabIndex = 0;
             btn_lert.Text = "Ler TXT";
             btn_lert.UseVisualStyleBackColor = true;
+            btn_lert.Click += btn_lerT_Click;
             // 
-            // listBox1
+            // lst_pessoas
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(46, 227);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(110, 34);
-            listBox1.TabIndex = 2;
+            lst_pessoas.FormattingEnabled = true;
+            lst_pessoas.Location = new Point(12, 116);
+            lst_pessoas.Name = "lst_pessoas";
+            lst_pessoas.Size = new Size(401, 169);
+            lst_pessoas.TabIndex = 2;
             // 
             // button1
             // 
-            button1.Location = new Point(28, 319);
+            button1.Location = new Point(214, 303);
             button1.Name = "button1";
             button1.Size = new Size(97, 20);
             button1.TabIndex = 0;
             button1.Text = "Guardar JSON";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += btn_guardarJ_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(28, 345);
+            button2.Location = new Point(317, 303);
             button2.Name = "button2";
             button2.Size = new Size(97, 20);
             button2.TabIndex = 0;
             button2.Text = "Ler JSON";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += btn_lerJ_Click;
             // 
             // txt_id
             // 
-            txt_id.Location = new Point(86, 32);
+            txt_id.Location = new Point(273, 63);
             txt_id.Name = "txt_id";
             txt_id.Size = new Size(104, 23);
             txt_id.TabIndex = 1;
             // 
             // txt_nome
             // 
-            txt_nome.Location = new Point(86, 72);
+            txt_nome.Location = new Point(68, 12);
             txt_nome.Name = "txt_nome";
             txt_nome.Size = new Size(104, 23);
             txt_nome.TabIndex = 1;
             // 
             // txt_idade
             // 
-            txt_idade.Location = new Point(86, 111);
+            txt_idade.Location = new Point(273, 12);
             txt_idade.Name = "txt_idade";
             txt_idade.Size = new Size(104, 23);
             txt_idade.TabIndex = 1;
             // 
             // txt_email
             // 
-            txt_email.Location = new Point(86, 150);
+            txt_email.Location = new Point(68, 60);
             txt_email.Name = "txt_email";
             txt_email.Size = new Size(104, 23);
             txt_email.TabIndex = 1;
@@ -119,7 +125,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(28, 32);
+            label1.Location = new Point(215, 63);
             label1.Name = "label1";
             label1.Size = new Size(18, 15);
             label1.TabIndex = 3;
@@ -128,7 +134,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(30, 72);
+            label2.Location = new Point(12, 12);
             label2.Name = "label2";
             label2.Size = new Size(40, 15);
             label2.TabIndex = 3;
@@ -137,7 +143,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(28, 111);
+            label3.Location = new Point(217, 12);
             label3.Name = "label3";
             label3.Size = new Size(36, 15);
             label3.TabIndex = 3;
@@ -146,22 +152,55 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(28, 150);
+            label4.Location = new Point(12, 63);
             label4.Name = "label4";
             label4.Size = new Size(36, 15);
             label4.TabIndex = 3;
             label4.Text = "Email";
             // 
+            // btn_converter_TJ
+            // 
+            btn_converter_TJ.Location = new Point(235, 329);
+            btn_converter_TJ.Name = "btn_converter_TJ";
+            btn_converter_TJ.Size = new Size(133, 20);
+            btn_converter_TJ.TabIndex = 4;
+            btn_converter_TJ.Text = "Converter JSON/TXT";
+            btn_converter_TJ.UseVisualStyleBackColor = true;
+            btn_converter_TJ.Click += btn_converter_JT_Click;
+            // 
+            // btn_converter_JT
+            // 
+            btn_converter_JT.Location = new Point(68, 329);
+            btn_converter_JT.Name = "btn_converter_JT";
+            btn_converter_JT.Size = new Size(133, 20);
+            btn_converter_JT.TabIndex = 4;
+            btn_converter_JT.Text = "Converter TXT/JSON";
+            btn_converter_JT.UseVisualStyleBackColor = true;
+            btn_converter_JT.Click += btn_converter_TJ_Click;
+            // 
+            // btn_maior_18
+            // 
+            btn_maior_18.Location = new Point(140, 355);
+            btn_maior_18.Name = "btn_maior_18";
+            btn_maior_18.Size = new Size(142, 20);
+            btn_maior_18.TabIndex = 5;
+            btn_maior_18.Text = "Maior 18";
+            btn_maior_18.UseVisualStyleBackColor = true;
+            btn_maior_18.Click += btn_maiores18_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(436, 391);
+            Controls.Add(btn_maior_18);
+            Controls.Add(btn_converter_JT);
+            Controls.Add(btn_converter_TJ);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(listBox1);
+            Controls.Add(lst_pessoas);
             Controls.Add(txt_email);
             Controls.Add(txt_idade);
             Controls.Add(txt_nome);
@@ -172,6 +211,7 @@
             Controls.Add(btn_guardat);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -180,7 +220,7 @@
 
         private Button btn_guardat;
         private Button btn_lert;
-        private ListBox listBox1;
+        private ListBox lst_pessoas;
         private Button button1;
         private Button button2;
         private TextBox txt_id;
@@ -191,5 +231,8 @@
         private Label label2;
         private Label label3;
         private Label label4;
+        private Button btn_converter_TJ;
+        private Button btn_converter_JT;
+        private Button btn_maior_18;
     }
 }
--- gerir_pessoas/gerir_pessoas/Form1.cs
+++ gerir_pessoas/gerir_pessoas/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace gerir_pessoas
 {
     public partial class Form1 : Form
@@ -6,7 +8,7 @@ namespace gerir_pessoas
         {
             InitializeComponent();
         }
-         //Guardar pessoas em ficheiro txt
+        //Guardar pessoas em ficheiro txt
         private void btn_guardat_Click(object sender, EventArgs e)
         {
             try
@@ -17,14 +19,204 @@ namespace gerir_pessoas
                 {
                     sw.WriteLine(p.ToString());
                 }
-                MessageBox.Show("Pessoa guardada com sucesso!");
+                MessageBox.Show("pessoa guardada com sucesso!");
             }
 
             catch (Exception ex)
             {
                 MessageBox.Show("Erro ao guardar pessoa: " + ex.Message);
             }
+        }
+        // Ler ficheiro TXT
+        private void btn_lerT_Click(object sender, EventArgs e)
+        {
 
+            lst_pessoas.Items.Clear();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("pessoas.txt"))
+                {
+                    string linha;
+
+                    // Ler linha a linha
+                    while ((linha = sr.ReadLine()) != null)
+                    {
+                        // Separar campos
+                        string[] dados = linha.Split(',');
+
+                        pessoa p = new pessoa(int.Parse(dados[0]), dados[1],
+                            int.Parse(dados[2]), dados[3]);
+
+                        lst_pessoas.Items.Add(p.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao ler ficheiros: " + ex.Message);
+            }
+        }
+
+        //Guardar em ficheiros JSON
+        private void btn_guardarJ_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pessoa p = new pessoa(int.Parse(txt_id.Text), txt_nome.Text,
+                    int.Parse(txt_idade.Text), txt_email.Text);
+
+                List<pessoa> pessoas = new List<pessoa>();
+
+                //Se o ficheiro já existe, ler dados anteriores
+                if (File.Exists("pessoas.json"))
+                {
+                    using (FileStream fs = new FileStream("pessoas.json", FileMode.Open))
+                    {
+                        pessoas = System.Text.Json.JsonSerializer.Deserialize<List<pessoa>>(fs);
+                    }
+                }
+
+                pessoas.Add(p);
+
+                // Guardar lista atualizada
+                using (FileStream fs = new FileStream("pessoas.json", FileMode.Create))
+                {
+                    System.Text.Json.JsonSerializer.Serialize(fs, pessoas, new JsonSerializerOptions { WriteIndented = true });
+                }
+
+                MessageBox.Show("pessoa guardada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+        }
+
+        private void btn_lerJ_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists("pessoas.json"))
+                {
+                    MessageBox.Show("Nenhum ficheiro encontrado.");
+                    return;
+                }
+                using (FileStream fs = new FileStream("pessoas.json", FileMode.Open))
+                {
+                    List<pessoa> pessoas = JsonSerializer.Deserialize<List<pessoa>>(fs);
+
+                    foreach (pessoa p in pessoas)
+                    {
+                        lst_pessoas.Items.Add(p.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+
+        }
+
+        private void btn_maiores18_Click(object sender, EventArgs e)
+        {
+            lst_pessoas.Items.Clear();
+
+            using (StreamReader sr = new StreamReader("pessoas.txt"))
+            {
+                string linha;
+                while ((linha = sr.ReadLine()) != null)
+                {
+                    string[] dados = linha.Split(";");
+                    int idade = int.Parse(dados[2]);
+
+                    if (idade == 18)
+                    {
+                        pessoa p = new pessoa(int.Parse(dados[0]), dados[1], idade, dados[3]);
+
+                        lst_pessoas.Items.Add(p.ToString());
+
+                    }
+                }
+            }
+        }
+
+        private void btn_converter_JT_Click(object sender, EventArgs e)
+        {
+            lst_pessoas.Items.Clear();
+
+            List<pessoa> listapessoas = new List<pessoa>();
+
+            using (StreamReader sr = new StreamReader("pessoas.json"))
+            {
+                string linha;
+                while ((linha = sr.ReadLine()) != null)
+                {
+                    string[] dados = linha.Split(";");
+
+                    pessoa p = new pessoa(
+                        int.Parse(dados[0]),
+                        dados[1],
+                        int.Parse(dados[2]),
+                        dados[3]);
+                    listapessoas.Add(p);
+                    lst_pessoas.Items.Add(p.ToString());
+                }
+                using (FileStream fs = new FileStream("pessoas.txt", FileMode.Create))
+                {
+                    JsonSerializer.Serialize(fs, listapessoas, new JsonSerializerOptions { WriteIndented = true });
+                }
+            }
+        }
+
+        private void btn_converter_TJ_Click(object sender, EventArgs e)
+        {
+            lst_pessoas.Items.Clear();
+
+            List<pessoa> listapessoas = new List<pessoa>();
+
+            using (StreamReader sr = new StreamReader("pessoas.txt"))
+            {
+                string linha;
+                while ((linha = sr.ReadLine()) != null)
+                {
+                    string[] dados = linha.Split(";");
+
+                    pessoa p = new pessoa(
+                        int.Parse(dados[0]),
+                        dados[1],
+                        int.Parse(dados[2]),
+                        dados[3]);
+                    listapessoas.Add(p);
+                    lst_pessoas.Items.Add(p.ToString());
+                }
+                using (FileStream fs = new FileStream("pessoas.json", FileMode.Create))
+                {
+                    JsonSerializer.Serialize(fs, listapessoas, new JsonSerializerOptions { WriteIndented = true });
+                }
+            }
+        } 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lst_pessoas.Items.Clear();
+
+            try
+            {
+                using(FileStream fs = new FileStream("pessoas.json", FileMode.Open))
+                {
+                    List<pessoa> pessoas = JsonSerializer.Deserialize<List<pessoa>>(fs);
+
+                    foreach (pessoa p in pessoas)
+                    {
+                        lst_pessoas.Items.Add(p.ToString());
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Não existem ficheiros criados" + ex.Message);
+            }
         }
     }
 }
